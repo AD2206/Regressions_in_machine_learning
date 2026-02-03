2b17337eb7ae214cc9ab3903d0a057f93fe95f21
Aryan Dubey <66486350+AD2206@users.noreply.github.com>
2026-02-03 16:03:39 +0530

    Add files via upload

---
+++ Salary_vs_years_of_expirience.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+#importing the data set
+dataset=pd.read_csv('Salary_Data.csv')
+X=dataset.iloc[:,:-1].values
+y=dataset.iloc[:,-1].values
+#splitting the dataset 
+from sklearn.model_selection import train_test_split 
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=0)
+#training the simple linear regression model on the training set 
+from sklearn.linear_model import LinearRegression
+regressor=LinearRegression()
+regressor.fit(X_train,y_train)
+#predicting the test results
+
+y_pred = regressor.predict(X_test)   #here predict method is making predictions 
+#visualizing training set results
+plt.scatter(X_train,y_train,color='red')
+plt.plot(X_train,regressor.predict(X_train),color='blue')
+plt.title('Salary vs Expirience  (Training Set)')
+plt.xlabel("Years of expirience")
+plt.ylabel("Salary")
+plt.show()
+
+
+#visualizing test set results
+
+
+
+plt.scatter(X_test,y_test,color='red')
+plt.plot(X_train,regressor.predict(X_train),color='blue')
+plt.title('Salary vs Expirience  (Training Set)')
+plt.xlabel("Years of expirience")
+plt.ylabel("Salary")
+plt.show()
+
