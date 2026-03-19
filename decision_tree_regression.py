0d4034ec21b12029f24dcb4935cba56fc791fe7f
Aryan Dubey <66486350+AD2206@users.noreply.github.com>
2026-03-19 20:34:11 +0530

    Create decision_tree_regression.py

---
+++ decision_tree_regression.py
@@ -0,0 +1,30 @@
+#importing the standard libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+#importing the dataset
+dataset=pd.read_csv('Position_Salaries.csv')
+X=dataset.iloc[:,1:-1].values
+y=dataset.iloc[:,-1].values
+#training the decision tree regression on the whole dataset
+from sklearn.tree import DecisionTreeRegressor 
+regressor=DecisionTreeRegressor(random_state=0)
+regressor.fit(X,y)
+#predicting the result
+regressor.predict([[6.5]])
+#visualising the results
+X_grid = np.arange(min(X[:,0]), max(X[:,0]), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+
+plt.title('Truth or Bluff (Decision Tree Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+from sklearn.metrics import r2_score
+
+y_pred = regressor.predict(X)
+print(r2_score(y, y_pred))
+print('rdr2')
